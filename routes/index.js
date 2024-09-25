a781f0297bf5933fff7b00f8b0877826d3e6b5dc
Usama Wajid <usama@livesports.ai>
2024-09-25 17:15:54 +0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
+const router = express.Router();
+
+module.exports = (params) => {
+  const { speakersService } = params;
+
+  router.get('/', async (request, response, next) => {
+    try {
+      const topSpeakers = await speakersService.getList();
+      const artwork = await speakersService.getAllArtwork();
+      response.render('layout', { pageTitle: 'Welcome', template: 'index', topSpeakers, artwork });
+    } catch (err) {
+      console.log(err);
+      return next(err);
+    }
+  });
+
+  router.use('/speakers', speakersRoute(params));
+  router.use('/feedback', feedbackRoute(params));
+
+  return router;
+};
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const path = require('path');
+const cookieSession = require('cookie-session');
+const createError = require('http-errors');
+const bodyParser = require('body-parser');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakersService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakersService = new SpeakersService('./data/speakers.json');
+
+const routes = require('./routes');
+
+const app = express();
+
+const port = 3000;
+
+app.set('trust proxy', 1);
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['Ghdur687399s7w', 'hhjjdf89s866799'],
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.locals.siteName = 'Roux Meetups';
+
+app.use(express.static(path.join(__dirname, './static'))); //Middleware
+
+app.use(async (request, response, next) => {
+  try {
+    const names = await speakersService.getNames();
+    response.locals.speakerName = names;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakersService,
+  })
+);
+
+app.use((request, response, next) => {  
+  return next(createError(404, 'File not found'));
+});
+
+app.use((err, request, response, next) => {
+  response.locals.message = err.message;
+  console.error(err);
+  const status = err.status || 500;
+  response.locals.status = status;
+  response.status(status);
+  response.render('error');
+});
+app.listen(port, () => {
+  console.log(`Express Server listening on port ${port}`);
+});
